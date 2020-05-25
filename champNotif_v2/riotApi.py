b67d6c0e9271dd117db8e4284fc57693ea5fded5
riv3n <vandamere@gmail.com>
2020-05-25 12:53:24 -0400

    champLookupTable

--- champNotif_v2/riotApi.py
+++ champNotif_v2/riotApi.py
@@ -3,6 +3,7 @@ from champNotif_v2 import app
 import json, requests, time
 from .database import query_db, get_db
 from flask import g
+from .champRotation import ChampRotation
 
 naEndpoint = "na1.api.riotgames.com"
 ddragonCdn = "http://ddragon.leagueoflegends.com/cdn"
@@ -10,7 +11,9 @@ api_key = app.config['API_KEY']
 dDragonVer = "10.8.1"
 champVer = "v3"
 champDataUrl = ddragonCdn + "/" + dDragonVer + "/data/en_US/champion.json"
-champion = "/api/lol/na/" + champVer + "/champion?api_key=" + api_key
+champRotationApi = "/lol/platform/v3/champion-rotations"
+champRotationUrl = naEndpoint + champRotationApi
+#champion = "/api/lol/na/" + champVer + "/champion?api_key=" + api_key
 
 #has key
 def getDataDict():
@@ -19,32 +22,23 @@ def getDataDict():
     jObj = r.json()
     return jObj['data']
 
-#has free attribute
-def getListOfChampDicts():
-    champApiUrl = "https://" + naEndpoint + champion
-    r = requests.get(champApiUrl)
+#gets a list of free champs; 
+#param: rotation enum: new player or standard rotation
+def getFreeChampRotation(rotationType): 
+    r = requests.get(champRotationUrl)
     jObj = r.json()
-    return jObj['champions']
+    if (rotationType == ChampRotation.NewPlayers):
+        return jObj['freeChampionIdsForNewPlayers']
+    return jObj['freeChampionIds']
 
-def getChampInfoById(id):
-    champApiUrl = "https://" + naEndpoint + "/api/lol/na/" + champVer + "/champion/" + str(id) + "?api_key=" + api_key
-    r = requests.get(champApiUrl)
-    return r.json()
-
-
-#could get 500 response
-def getChampById(id):
-    response = requests.get("https://" + naEndpoint + "/api/lol/na/" + champVer + "/champion/" + str(id) + "?api_key=" + api_key)
-    dict = response.json()
-    return dict['name']
-
-
-def getListOfChamps():
-    champList = []
-    objOfLists = getListOfChampDicts()
-    for dict in objOfLists:
-        champList.append(getChampById(str(dict['key'])))
-    return champList
+#returns a dictionary with champ id as the key and champion name as the value
+def createChampLookupTable():
+    champLookup = {}
+    champDicts = getDataDict()
+    for name, champDict in champDicts.items():
+        id = champDict["key"]
+        champLookup[id] = name
+    return champLookup
 
 #get the latest image repo version in order to render the latest champ images
 def getDragonVer():
@@ -52,9 +46,6 @@ def getDragonVer():
     response = requests.get(url)
     jObj = response.json()
     return jObj[0]
-
-
-
 
 def main():
     data = getDragonVer()
